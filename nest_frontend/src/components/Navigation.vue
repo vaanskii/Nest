2b65c094fef165106b2892df1625581a895c815d
vaanskii <giorgivanadze03@gmail.com>
2024-03-20 17:48:27 +0400

    commit change

--- nest_frontend/src/components/Navigation.vue
+++ nest_frontend/src/components/Navigation.vue
@@ -26,8 +26,8 @@
               </div>
 
 
-              <div v-if="isBurgerVisible" class="absolute inset z-40 h-full mt-[990px] w-full bg-white">
-                <div class="flex flex-col gap-5 justify-center items-center" v-if="userStore.user.isAuthenticated">
+              <div v-if="isBurgerVisible" class="absolute inset z-40 h-full mt-[850px] w-full bg-white">
+                <div class="flex flex-col gap-5 justify-center items-center mt-40" v-if="userStore.user.isAuthenticated">
                   <RouterLink to="/" class="flex justify-center items-center" @click="toggleBurger()">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10 text-black">
                       <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
