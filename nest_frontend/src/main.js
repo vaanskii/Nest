b574ee4a38b9f6059f1db1fd9c7477ad2c792de4
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-08-27 14:49:36 +0400

    commit changes.

--- nest_frontend/src/main.js
+++ nest_frontend/src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+axios.defaults.baseURL = "https://vaanskii.pythonanywhere.com"
 const app = createApp(App)
 
 app.use(createPinia())
